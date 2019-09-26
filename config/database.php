2959432c33a5a005eae2be148753f23eb6e8a524
jacobnehme <39905774+jacobnehme@users.noreply.github.com>
2019-09-26 10:39:01 +0200

    JN Dev Init

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
         ],
 
         'pgsql' => [
