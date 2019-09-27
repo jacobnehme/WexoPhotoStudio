45a7b5f361d9cc5739eedce99640fb75fecc6e95
jacobnehme <39905774+jacobnehme@users.noreply.github.com>
2019-09-27 12:42:52 +0200

    Auth init

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::resource('orders', 'OrderController');
 
 Route::resource('photos', 'PhotoController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
