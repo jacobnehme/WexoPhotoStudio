7b01867f560eaae55a2bc4db09aa8c33f6484e01
jacobnehme <39905774+jacobnehme@users.noreply.github.com>
2019-10-01 17:06:53 +0200

    changed index page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'ProductController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
